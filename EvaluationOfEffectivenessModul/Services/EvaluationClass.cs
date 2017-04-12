cc346ee2c97a3b4b4a31c2517e18eb02b9ae1b63
d.gerasymiuk <d.gerasymiuk@gmail.com>
2017-04-12 21:48:00 +0300

    add Method to Evaluation Class

--- EvaluationOfEffectivenessModul/Services/EvaluationClass.cs
+++ EvaluationOfEffectivenessModul/Services/EvaluationClass.cs
@@ -12,11 +12,11 @@ namespace EvaluationOfEffectivenessModul.Services
         {
             return E/Y;
         }
-        private double getEfficiencyCost(double e, long b)
+        private double getEfficiencyCost(double e, long b)//2
         {
             return e / b;
         }
-        private double getSumSingleRiskList(IList<double> p, IList<long> u)
+        private double getSumSingleRiskList(IList<double> p, IList<long> u)//3
         {
             int n = p.Count;
             double res = 0;
@@ -26,18 +26,33 @@ namespace EvaluationOfEffectivenessModul.Services
             }
             return res;
         } 
-        private double getRisk(Int32 E, Int32 M)
+        private double getRisk(Int32 E, Int32 M)//4
         {
             return E / M;
         }
-        private double getAgregateRisk(List<double> p, List<long> u, Int32 E, Int32 M)
+        private double getAgregateRisk(List<double> p, List<long> u, Int32 E, Int32 M)//5
         {
             return getSumSingleRiskList(p, u)+getRisk(E,M);
         }
-        private double getRang(long sum, double p, long cost)
+        private double getRang(long sum, double p, long cost)//6
         {
             return (sum * p) / cost;
         }
+        private double getCost(double D, long I, long W)//7
+        {
+            return D + I * W;
+        }
+        private double getCoefficientRentabilnosti(IList<long> CF, long k)//8
+        {
+            double res = 0;
+            int i = 1;
+            foreach(long item in CF){
+                res+=item/Math.Pow((1+k),i);
+                i++;
+            }
+            return res;
+        }
+        
 
     }
 }
